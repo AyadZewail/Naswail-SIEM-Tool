13d0159621e7907aff4789d8e99eef3c1c2d1052
Ayad Zewail <130586350+AyadZewail@users.noreply.github.com>
2025-04-11 21:24:52 +0200

    Update and rename packeto_sendito.py to packeeto_sendito.py

--- packeeto_sendito.py
+++ packeeto_sendito.py
@@ -1,5 +1,4 @@
 from scapy.all import IP, TCP, send
 
-# Craft a packet with spoofed source IP
 packet = IP(src="203.0.113.42", dst="192.168.0.129") / TCP(sport=1234, dport=80, flags="S")
-send(packet, count=5)  # Source IP will appear as 10.0.0.100
+send(packet, count=5)
